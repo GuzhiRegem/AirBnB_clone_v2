0eac41011f370a133c964f9f11182012ca91f6e3
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-13 22:42:26 -0300

    commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+sudo apt update
+sudo apt -y install nginx
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test
@@ -10,5 +12,6 @@ echo '<html>
   </body>
 </html>' > /data/web_static/releases/test/index.html
 ln -s /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu /data
-chgrp -R ubuntu /data
+chown -hR ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
