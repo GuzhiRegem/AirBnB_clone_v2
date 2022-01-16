c4c2b61d458e9a7e735e47f41ac45d1827e87f20
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-16 20:29:11 -0300

    commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """
     module
 """
-from fabric.api import run, env, put
+from fabric.api import run, env, put, sudo
 import os
 
 env.hosts = ['34.74.218.204', '34.138.156.219']
@@ -17,13 +17,13 @@ def do_deploy(archive_path):
         return False
     try:
         put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(rel_d))
-        run('tar -xzf {} -C {}'.format())
-        run('rm {}'.format(tmp_d))
-        run('mv {}web_static/* {}'.format(rel_d, rel_d))
-        run('rm -rf {}web_static'.format(rel_d))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(rel_d))
+        sudo('mkdir -p {}'.format(rel_d))
+        sudo('tar -xzf {} -C {}'.format())
+        sudo('rm {}'.format(tmp_d))
+        sudo('mv {}web_static/* {}'.format(rel_d, rel_d))
+        sudo('rm -rf {}web_static'.format(rel_d))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {} /data/web_static/current'.format(rel_d))
         print('deployed')
         return True
     except BaseException:
