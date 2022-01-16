24fc023ea978346dd4bb8ef557ea2c4ced136021
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-16 13:37:43 -0300

    commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 
+    module
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ do_ pack """
+    tim = datetime.now().strftime('%Y%m%d%H%M%S')
+    local('mkdir -p versions')
+    local('tar -cvzf "versions/web_static_{}.tgz" web_static'.format(tim))
