39eb6696aaa0095e1149d3d69ed72ac9f5fd8026
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-16 14:37:12 -0300

    commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ env.hosts = ['34.74.218.204', '34.138.156.219']
 
 
 def do_deploy(archive_path):
+    """ do_deploy """
     filename = archive_path.split('/')[-1][:-4]
     tmp_d = '/tmp/{}.tgz'.format(filename)
     rel_d = '/data/web_static/releases/' + filename + '/'
@@ -25,9 +26,5 @@ def do_deploy(archive_path):
         run('ln -s {} /data/web_static/current'.format(rel_d))
         print('deployed')
         return True
-    except:
+    except BaseException:
         return False
-
-
-
-
