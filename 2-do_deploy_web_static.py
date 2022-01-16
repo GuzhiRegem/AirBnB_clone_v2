1e0cc26929cdde11ce9dc80bd614de26bd2462b8
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-16 14:34:59 -0300

    commit

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    module
+"""
+from fabric.api import run, env, put
+import os
+
+env.hosts = ['34.74.218.204', '34.138.156.219']
+
+
+def do_deploy(archive_path):
+    filename = archive_path.split('/')[-1][:-4]
+    tmp_d = '/tmp/{}.tgz'.format(filename)
+    rel_d = '/data/web_static/releases/' + filename + '/'
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(rel_d))
+        run('tar -xzf {} -C {}'.format())
+        run('rm {}'.format(tmp_d))
+        run('mv {}web_static/* {}'.format(rel_d, rel_d))
+        run('rm -rf {}web_static'.format(rel_d))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(rel_d))
+        print('deployed')
+        return True
+    except:
+        return False
+
+
+
+
