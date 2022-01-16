63a11af33d4995085a1c158823022882e0cd6041
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-16 20:13:15 -0300

    commit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,6 +8,11 @@ from datetime import datetime
 
 def do_pack():
     """ do_ pack """
-    tim = datetime.now().strftime('%Y%m%d%H%M%S')
-    local('mkdir -p versions')
-    local('tar -cvzf "versions/web_static_{}.tgz" web_static'.format(tim))
+    try:
+        tim = datetime.now().strftime('%Y%m%d%H%M%S')
+        name = "versions/web_static_{}.tgz".format(tim)
+        local('mkdir -p versions')
+        local('tar -cvzf "{}" web_static'.format(name))
+        return name
+    except Exception:
+        return None
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    module
+"""
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+def deploy():
+    """ deploy """
+    ret = do_pack()
+    if not ret:
+        return False
+    return do_deploy(ret)
