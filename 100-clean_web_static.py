da152b78b92f0d9d72126d34e2968a1698a5e459
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-16 21:07:55 -0300

    commit

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,6 +12,6 @@ env.hosts = ['34.74.218.204', '34.138.156.219']
 def do_clean(number=0):
     """ do_ clean """
     number = 1 if int(number) == 0 else int(number)
-    outp = sudo("ls -1 /data/web_static/releases")
+    outp = sudo('ls -1 /data/web_static/releases | grep "web_static"')
     print(outp)
     print(outp.split('\n'))
