2515ffd2d7f43666ac077a154f2f0615334c8747
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-16 22:16:17 -0300

    commit

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,16 +13,24 @@ def do_clean(number=0):
     """ do_ clean """
     number = 1 if int(number) == 0 else int(number)
     comms = [
-        'ls -1t versions',
+        'ls -1t',
         'grep "web_static"',
         "head -n{}".format(number)
     ]
-    outp = local("|".join(comms), capture=True)
-    lis = outp.split('\n')
-    for ind, val in enumerate(lis):
-        if val[-1] == "\r":
-            lis[ind] = val[:-1]
     with lcd("versions"):
-        sudo('rm -rv !("{}")'.format('"|"'.join(lis)))
+        lis = local("|".join(comms), capture=True).split('\n')
+        for ind, val in enumerate(lis):
+            lis[ind] = val[:-1] if val[-1] == "\r" else val
+        l_lis = local("|".join(comms[:-1]), capture=True).split('\n')
+        for ind, val in enumerate(l_lis):
+            l_lis[ind] = val[:-1] if val[-1] == "\r" else val
+        for a in l_lis:
+            if a not in lis:
+                local('rm -r {}'.format(a))
     with cd("/data/web_static/releases/"):
-        sudo('rm -rv !("{}")'.format('"|"'.join(lis)))
+        l_lis = local("|".join(comms[:-1]), capture=True).split('\n')
+        for ind, val in enumerate(l_lis):
+            l_lis[ind] = val[:-1] if val[-1] == "\r" else val
+        for a in l_lis:
+            if a not in lis:
+                sudo('rm -r {}'.format(a))
