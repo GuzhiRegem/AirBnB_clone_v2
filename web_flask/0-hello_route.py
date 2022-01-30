26f9752efe885fc1fe26d601fc7daaec900c7f5e
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-01-30 15:34:38 -0300

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-    
+"""    
     module
-    return: nothing
 """
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_world():
+    """ hello """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
